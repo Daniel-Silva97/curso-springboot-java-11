d4d99144ab5b7e38f3b3fc8b7e993bbb6f623d2c
Daniel da silva pedro <danielsp699@outlook.com>
2022-09-23 18:45:36 -0300

    Exception handling - delete

--- src/main/java/com/springjpa/course/services/UserService.java
+++ src/main/java/com/springjpa/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.springjpa.course.services;
 
 import com.springjpa.course.entities.User;
 import com.springjpa.course.repositories.UserRepository;
+import com.springjpa.course.services.exceptions.DatabaseException;
 import com.springjpa.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
